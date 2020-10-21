0bfbb02445e27c210cbc65ecc94142ceb8115d8f
sage90180 <sage90180@gmail.com>
2020-10-21 19:14:03 +0800

    後端切版

--- migrations/20201021063308-create-dish.js
+++ migrations/20201021063308-create-dish.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       TypeId: {
-        type: Sequelize.STRING
+        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
